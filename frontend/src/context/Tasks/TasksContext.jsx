142c6f76a5f0f6ec75804e422aa869906b7f088e
Eric-Zanette <ericzanette@gmail.com>
2023-08-27 20:50:10 -0400

    routing

--- frontend/src/context/Tasks/TasksContext.jsx
+++ frontend/src/context/Tasks/TasksContext.jsx
@@ -1,72 +1,78 @@
-import{createContext, useState, useContext} from 'react'
-import UserContext from '../Users/UserContext'
-import axios from 'axios'
+import { createContext, useState, useContext } from "react";
+import UserContext from "../Users/UserContext";
+import axios from "axios";
 
-const TasksContext = createContext()
+const TasksContext = createContext();
 
-export const TasksProvider = ({children}) => {
-  const [tasks, setTasks] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+export const TasksProvider = ({ children }) => {
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
 
-  const localToken = localStorage.getItem('user')
-  axios.defaults.headers.common['Authorization'] = localToken
-
+  const localToken = localStorage.getItem("user");
+  axios.defaults.headers.common["Authorization"] = localToken;
 
   const fetchTasks = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      if(user) {
-        const response = await axios.get('http://localhost:5000/api/tasks')
-        setTasks(response.data)
-        }
-    } catch(error) {
-      console.log(error)
-      setTasks([])
+      if (user) {
+        const response = await axios.get("/api/tasks");
+        setTasks(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+      setTasks([]);
     }
-    setIsLoading(false)
-  }
+    setIsLoading(false);
+  };
 
   const addTask = async (task) => {
     try {
-      if(user) {
-        await axios.post('http://localhost:5000/api/tasks', task)
-        fetchTasks()
-        }
-    } catch(error) {
-      console.log(error)
+      if (user) {
+        await axios.post("/api/tasks", task);
+        fetchTasks();
+      }
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   const editTask = async (task) => {
     try {
-      if(user) {
-        await axios.post(`http://localhost:5000/api/tasks/${task._id}`, task)
-        fetchTasks()
-        }
-    } catch(error) {
-      console.log(error)
+      if (user) {
+        await axios.post(`/api/tasks/${task._id}`, task);
+        fetchTasks();
+      }
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   const deleteTask = async (task) => {
-    try{
-      await axios.delete(`http://localhost:5000/api/tasks/${task._id}`)
-      fetchTasks()
-    } catch(error){
-      console.log(error)
+    try {
+      await axios.delete(`/api/tasks/${task._id}`);
+      fetchTasks();
+    } catch (error) {
+      console.log(error);
     }
-    
-  }
-
+  };
 
   return (
-    <TasksContext.Provider value={{tasks, fetchTasks, addTask, editTask, deleteTask, isLoading, setIsLoading}}>
-        {children}
+    <TasksContext.Provider
+      value={{
+        tasks,
+        fetchTasks,
+        addTask,
+        editTask,
+        deleteTask,
+        isLoading,
+        setIsLoading,
+      }}
+    >
+      {children}
     </TasksContext.Provider>
-    
-  )
-}
+  );
+};
 
-export default TasksContext
+export default TasksContext;
--- frontend/src/context/Users/UserContext.jsx
+++ frontend/src/context/Users/UserContext.jsx
@@ -1,72 +1,69 @@
 import { createContext, useState, useEffect } from "react";
-import axios from 'axios'
-import jwt_decode from 'jwt-decode'
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
-const UserContext = createContext()
+const UserContext = createContext();
 
-export const UserProvider = ({children}) => {
-    let localToken = localStorage.getItem('user')
+export const UserProvider = ({ children }) => {
+  let localToken = localStorage.getItem("user");
 
-    const [user, setUser] = useState(localToken &&
-        jwt_decode(
-        localStorage.getItem('user')
-        ))
-   
-    const isExpired = (token) => {
-        try{
-            const decodedToken = jwt_decode(token)
-            const now = Math.floor(Date.now() / 1000)
-            if(now > decodedToken.exp) {localStorage.removeItem('user')}
-            
-        } catch {
-            return false
-        }
-    }
+  const [user, setUser] = useState(
+    localToken && jwt_decode(localStorage.getItem("user"))
+  );
 
-    useEffect(() => {
-        isExpired(localToken)
-        axios.defaults.headers.common['Authorization'] = localToken
-    }, [localToken])
-
-    
-    const registerUser = async (newUser) => {
-        try {
-            await axios.post('http://localhost:5000/api/users/register', newUser)
-            return true
-        } catch(error) {
-
-            return error.response.data
-        }
+  const isExpired = (token) => {
+    try {
+      const decodedToken = jwt_decode(token);
+      const now = Math.floor(Date.now() / 1000);
+      if (now > decodedToken.exp) {
+        localStorage.removeItem("user");
       }
-    
-    const loginUser = async(user) => {
-        try{
-            const response = await axios.post('http://localhost:5000/api/users/login', user)
-            const token = response.data.token
-            console.log(token)
-
-            setUser(jwt_decode(token))
-            localStorage.setItem('user', token)
-            axios.defaults.headers.common['Authorization'] = token
-
-            return true
-        } catch(error){
-            console.log(error.response.data)
-            return error.response.data
-        }
+    } catch {
+      return false;
     }
+  };
 
-    const logout = async () => {
-        setUser(null)
-        localStorage.removeItem('user')
-    }   
+  useEffect(() => {
+    isExpired(localToken);
+    axios.defaults.headers.common["Authorization"] = localToken;
+  }, [localToken]);
 
+  const registerUser = async (newUser) => {
+    try {
+      await axios.post("api/users/register", newUser);
+      return true;
+    } catch (error) {
+      return error.response.data;
+    }
+  };
 
-    return(
-        <UserContext.Provider value={{user, registerUser, logout, loginUser}}>
-            {children}
-        </UserContext.Provider>
-        )
-}
+  const loginUser = async (user) => {
+    try {
+      const response = await axios.post("api/users/login", user);
+      const token = response.data.token;
+      console.log(token);
 
-export default UserContext
+      setUser(jwt_decode(token));
+      localStorage.setItem("user", token);
+      axios.defaults.headers.common["Authorization"] = token;
+
+      return true;
+    } catch (error) {
+      console.log(error.response.data);
+      return error.response.data;
+    }
+  };
+
+  const logout = async () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <UserContext.Provider value={{ user, registerUser, logout, loginUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
